af468fde45a716235493fc7a09fba2265d9754cb
shaleenjain28 <jainshaleen2005@gmail.com>
2026-06-03 04:02:33 +0530

    added the User repository layer

---
+++ backend/src/main/java/com/sochupi/app/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.sochupi.app.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sochupi.app.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
