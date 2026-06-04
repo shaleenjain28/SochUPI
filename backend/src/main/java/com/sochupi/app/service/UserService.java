f0834e676a1ba06f1620bc5a34243b20004f3a4d
shaleenjain28 <jainshaleen2005@gmail.com>
2026-06-04 22:35:56 +0530

    added the password hashing to store in db

--- backend/src/main/java/com/sochupi/app/service/UserService.java
+++ backend/src/main/java/com/sochupi/app/service/UserService.java
@@ -7,25 +7,34 @@ import com.sochupi.app.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 @RequiredArgsConstructor
+
 public class UserService {
     // injecting the repository
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserResponse registerUser(RegisterRequest req) {
 
+        // 1. Check if email exists
         Optional<User> existingUser = userRepository.findByEmail(req.email());
         if (existingUser.isPresent()) {
             throw new RuntimeException("User already exists");
         }
+
+        // 2. Create the entity
         User newUser = new User();
         newUser.setName(req.name());
         newUser.setEmail(req.email());
-        // TODO: replace this with real BCrypt hashing later
-        newUser.setPasswordHash(req.password());
 
+        // TODO: We will replace this with real BCrypt hashing later!
+        String passwordHash = passwordEncoder.encode(req.password());
+        newUser.setPasswordHash(passwordHash);
+
+        // 3. Save to database (capture the returned object!)
         User savedUser = userRepository.save(newUser);
         return new UserResponse(
                 savedUser.getId(),
